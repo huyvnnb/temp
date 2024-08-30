07cbc8b81ef98e79b774c3423a0a5cb5706834ae
huyvnnb <huyqktk@gmail.com>
2024-08-30 13:33:53 +0700

    my portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const h1Element = document.querySelector('.home-content h1');
+    const h3Element = document.querySelector('.home-content h3');
+  
+    h1Element.style.animation = 'typing 2s steps(50, end) infinite';
+    h3Element.style.animation = 'typing 2s steps(50, end) infinite';
+});
